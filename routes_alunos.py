b232396a8cdd7f3be6b96bbe1c946c19d3163565
Anderson Rieger <50467500+arluz@users.noreply.github.com>
2026-06-25 21:08:35 +0000

    routes para Alunos criada e funcionando

--- routes_alunos.py
+++ routes_alunos.py
@@ -1,39 +1,16 @@
-from flask import Flask, request, render_template, redirect, url_for, abort, flash
+from flask import Flask, Blueprint, request, render_template, redirect, url_for, abort, flash
 from sqlalchemy.exc import IntegrityError
 import models
+import re
 
-from routes_alunos import alunos_bp
+alunos_bp = Blueprint('alunos', __name__, url_prefix='/alunos') 
 
-app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:12345678@localhost:5432/dados_cic"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-app.secret_key = "your_secret_key"  # Substitua por uma chave secreta real em produção
-
-models.db.init_app(app)
-
-app.register_blueprint(alunos_bp)
-
-with app.app_context():
-    models.db.create_all()
-
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-@app.errorhandler(404)
-def page_not_found(e):
-    mensagem = getattr(e, 'description', 'Página não encontrada')
-    return render_template('errors/404.html', mensagem=mensagem), 404
-
-"""
-@app.route("/alunos")
+@alunos_bp.route("")
 def list_alunos():
     alunos = models.Alunos.query.all()
-    return render_template('Alunos/list_alunos.html', alunos=alunos)
+    return render_template('/Alunos/list_alunos.html', alunos=alunos)
 
-@app.route("/alunos/create", methods=['GET', 'POST'])
+@alunos_bp.route("/create", methods=['GET', 'POST'])
 def create_alunos():
     if request.method == 'GET':
         return render_template('Alunos/create_alunos.html')
@@ -51,21 +28,21 @@ def create_alunos():
             models.db.session.add(aluno)
             models.db.session.commit()
             flash(f"Aluno {nome} criado com sucesso!", "success")
-            return redirect(url_for('list_alunos'))
+            return redirect(url_for('alunos.list_alunos'))
         
         except IntegrityError:
             models.db.session.rollback()
             flash(f"Erro ao criar aluno {nome}. Esta Matrícula já existe.", "error")
             return render_template('Alunos/create_alunos.html')
         
-@app.route("/alunos/<int:id_aluno>")
+@alunos_bp.route("/<int:id_aluno>")
 def get_aluno(id_aluno):
     aluno = models.Alunos.query.get(id_aluno)
     if aluno is None:
         abort(404, description="Aluno não encontrado")
     return render_template('Alunos/get_aluno.html', aluno=aluno)
 
-@app.route("/alunos/<int:id_aluno>/update", methods=['GET', 'POST'])
+@alunos_bp.route("/<int:id_aluno>/update", methods=['GET', 'POST'])
 def update_aluno(id_aluno):
     aluno = models.Alunos.query.get(id_aluno)
     if aluno is None:
@@ -79,9 +56,9 @@ def update_aluno(id_aluno):
         aluno.matricula = request.form.get('matricula')
         models.db.session.commit()
         flash(f"Aluno {aluno.nome} atualizado com sucesso!", "success")
-        return redirect(url_for('list_alunos'))
+        return redirect(url_for('alunos.list_alunos'))
 
-@app.route("/alunos/<int:id_aluno>/delete", methods=['GET', 'POST'])
+@alunos_bp.route("/<int:id_aluno>/delete", methods=['GET', 'POST'])
 def delete_aluno(id_aluno):
     aluno = models.Alunos.query.get(id_aluno)
     if aluno is None:
@@ -94,13 +71,8 @@ def delete_aluno(id_aluno):
         models.db.session.delete(aluno)
         models.db.session.commit()
         flash(f"Aluno {aluno.nome} deletado com sucesso!", "success")
-        return redirect(url_for('list_alunos'))
+        return redirect(url_for('alunos.list_alunos'))
     except IntegrityError:
         models.db.session.rollback()
         flash(f"Erro ao deletar aluno {aluno.nome}.", "error")
-        return redirect(url_for('get_aluno', id_aluno=id_aluno))
-        
-"""
-
-if __name__ == "__main__":
-    app.run(debug=True)
+        return redirect(url_for('alunos.get_aluno', id_aluno=id_aluno))
